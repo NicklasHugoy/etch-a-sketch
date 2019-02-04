7d307ef973265078be5f4cbb8e36ded48ee869da
NicklasHugoy <nicklas.hug@gmail.com>
2019-02-04 11:51:15 +0100

    Dynamic grid. Uses float instead of css grid

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-const container = document.querySelector(".container");
+createGrid(16);
 
-let counter = 1;
+function createGrid(grid_size){
+    const container = document.querySelector(".container");
+    clearGrid(container);
+    
+    for(i = 0; i<grid_size; i++){
+        for(j = 0; j<grid_size; j++){
+            let grid_element = document.createElement('div');
+            grid_element.classList.add('grid_element');
 
-for(i = 0; i<16; i++){
-    for(j = 0; j<16; j++){
-        let grid_element = document.createElement('div');
-        grid_element.classList.add('grid_element');
-        
-        grid_element.textContent = counter;
-        counter += 1;
-        
-        container.appendChild(grid_element);
+            let block_size = 960/grid_size;
+            grid_element.style.height = block_size + 'px';
+            grid_element.style.width = block_size + 'px';
+
+            if(j == 0){
+                grid_element.style.clear = "left";
+            }
+
+            container.appendChild(grid_element);
+        }
     }
 }
+
+function clearGrid(container){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function newGrid(){
+    let grid_size = prompt("Plese enter size for new grid");
+    createGrid(grid_size);
+}
+
+const new_grid_button = document.querySelector('#new_grid_button');
+new_grid_button.addEventListener('click', newGrid);
